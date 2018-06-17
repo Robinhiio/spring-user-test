f680aa3ebbdca80e345dcaadecf3db158ec546ce
Robin Larsson <Robinhiio@hotmail.com>
2018-06-17 13:11:22 +0200

    s

---
+++ src/main/java/com/example/springusertest/SpringUserTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.springusertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUserTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUserTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springusertest/SpringUserTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springusertest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringUserTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
